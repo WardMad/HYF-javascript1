13d2d9ea3383e922b29ef98160baee9063cc7287
WArd <32795132+WardMad@users.noreply.github.com>
2018-07-05 17:01:10 +0200

    Delete 4_exercise.js

--- 4_exercise.js
+++
@@ -1,7 +0,0 @@
-var y = "i wanna vacation";
-  console.log("the value of my variable x will be: 'i wanna vacation'");
-  console.log(y);
-
-y = "somwhere in Italy";
-  console.log("the value of my variable x will be: 'somewhere in Italy'");
-  console.log(y);
